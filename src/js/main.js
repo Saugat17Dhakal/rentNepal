0b5d3a737f0d4eab864b58dc289f7b47273c7dc2
Saugat17Dhakal <saugatdhakal19@gmail.com>
2024-01-05 21:01:36 +0545

    first commit

--- src/js/main.js
+++ src/js/main.js
@@ -1,14 +1,75 @@
-const navbar = document.querySelector(".navbar-nav")
-const openBtn = document.querySelector(".menu-btn")
-const closeBtn = document.querySelector(".close-btn")
-console.log("Hello, World!")
-openBtn.addEventListener("click", () => {
-    navbar.classList.add("active")
-    openBtn.classList.remove("active")
-    closeBtn.classList.add("active")
-})
-closeBtn.onclick = () => {
-    navbar.classList.remove("active")
-    openBtn.classList.add("active")
-    closeBtn.classList.remove("active")
+//LOADER
+window.onload = () => {
+    const preloader = document.querySelector(".loader")
+    preloader.classList.add("load-finish")
+}
+// NAVBAR DROPDOWN
+const navBarBottomNavLanguageDropdownOpener = document.querySelector(".navbar-bottom-nav-language")
+const navBarBottomNavLanguageDropdown = document.querySelector(".navbar-bottom-nav-language-dropdown");
+navBarBottomNavLanguageDropdownOpener.onclick = () => {
+    navBarBottomNavLanguageDropdown.classList.toggle("active")
+}
+// HERO SECTION SLIDER
+const slider = document.querySelector(".slider-images")
+const sliderImages = document.querySelectorAll(".slide")
+const prevBtn = document.querySelector(".prev")
+const nextBtn = document.querySelector(".next")
+const currentSlideDisplay = document.querySelector(".currentSlideDisplay")
+const totalSlideDisplay = document.querySelector(".totalSlideDisplay")
+let currentSlide = 0
+prevBtn.onclick = () => {
+    currentSlide--
+    if(currentSlide == -1){
+        currentSlide = sliderImages.length - 1
+        slider.style.transform = `translateX(${-(currentSlide) * 100}%)`
+        currentSlideDisplay.innerHTML = `${currentSlide + 1}`
+    }
+    else{
+        slider.style.transform = `translateX(${-(currentSlide) * 100}%)`
+        currentSlideDisplay.innerHTML = `${currentSlide + 1}`
+    }
+}
+nextBtn.onclick = () => {
+    currentSlide++
+    if(currentSlide == sliderImages.length){
+        currentSlide = 0
+        slider.style.transform = `translateX(${-(currentSlide) * 100}%)`
+        currentSlideDisplay.innerHTML = `${currentSlide + 1}`
+    }
+    else{
+        slider.style.transform = `translateX(${-(currentSlide) * 100}%)`
+        currentSlideDisplay.innerHTML = `${currentSlide + 1}`
+    }
+}
+setInterval( () => {
+        currentSlide++;
+        if(currentSlide == sliderImages.length){
+            currentSlide = 0
+            slider.style.transform = `translateX(${-(currentSlide) * 100}%)`
+            currentSlideDisplay.innerHTML = `${currentSlide + 1}`
+        }
+        else{
+            slider.style.transform = `translateX(${-(currentSlide) * 100}%)`
+            currentSlideDisplay.innerHTML = `${currentSlide + 1}`
+        }
+}, 2000)
+
+currentSlideDisplay.innerHTML = `${currentSlide + 1}`
+totalSlideDisplay.innerHTML = `${sliderImages.length}`
+
+// DARKMODE JS AND STORE USING LOCAL STORAGE
+const changeColor = document.querySelector('.navbar-bottom-nav-darkmode')
+const body = document.body;
+const currentTheme = localStorage.getItem('currentTheme')
+if(currentTheme){
+    body.classList.add('darkmode')
+}
+changeColor.onclick = () => {
+    body.classList.toggle('darkmode')
+    if(body.classList.contains('darkmode')){
+            localStorage.setItem('currentTheme', 'enabled')
+    }
+    else{
+            localStorage.removeItem('currentTheme')
+    }
 }
